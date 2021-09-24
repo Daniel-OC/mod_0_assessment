99b5471200be640d2cf6dcbb865eac9add6bf3ce
Daniel O'Connell <danieloconnell.maker@gmail.com>
2021-09-24 13:43:52 -0700

    Complete Program Specific Challenge

---
+++ variables.js
@@ -0,0 +1,15 @@
+var movie = "I Heart Huckabees"
+var isEntertaining = true
+
+movie = "Speed 2: Cruise Control"
+
+var num4 = 6
+var num6 = 4
+
+var product = num4 * num6
+
+var favoriteFoods = ["Burgers", "Ice Cream", "Tacos"]
+
+favoriteFoods.shift();
+
+console.log(movie.length);
