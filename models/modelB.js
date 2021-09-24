4d517c9254f0ac1fb58200644e62a872246ca8ac
Daniel O'Connell <danieloconnell.maker@gmail.com>
2021-09-24 13:58:38 -0700

    Add developer model

---
+++ models/modelB.js
@@ -0,0 +1,11 @@
+var cyrusJones = {
+  specialty: "Ruby",
+  dogsName: "Scoober",
+  age: 103,
+  favoriteNumber: 103,
+  hasGrandkids: true,
+  livedForever: true,
+  bestFriends: ["Billi", "Pamm", "Suze"],
+  worstEnemies: ["Billy", "Pam", "Susie"]
+
+}
