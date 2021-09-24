24d6a5c892b39d6e229f96dd6512bf0ae60cb3d0
Daniel O'Connell <danieloconnell.maker@gmail.com>
2021-09-24 13:53:53 -0700

    Add dinosaur model

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var stegosaurus = {
+  name: "Steig",
+  favoriteFood: "Big ol' leafs",
+  numberOfSpikes: 4,
+  numberOfFriends: 400000,
+  isSpiky: true,
+  isCarnivore: false,
+  bestFriends: ["Greg","Bobbi", "Susie"],
+  worstEnemies: ["Gregg", "Bobby", "Suze"]
+};
